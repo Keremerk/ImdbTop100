09603cad122653fb4c71c212e38cc30598bd1e1f
Kerem Erkubilay <kereme@windowslive.com>
2023-03-29 17:02:38 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        println("ss")
         val layoutManager = LinearLayoutManager(this)
         binding.RecyclerView.layoutManager = layoutManager
         moviesAdapter = MoviesAdapter(movieList)
